1796f01ea67e80ac87317bf848a671b5829882ad
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-22 13:43:18 -0400

    init package.json, added express dependency, created a server listen

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running ~ :)");
+});
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
