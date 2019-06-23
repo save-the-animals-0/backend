9de605ce9dafba812ee25c1b72ae6225c81b2538
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-23 17:19:06 -0400

    added mongoose dependencies and set up a base for the mongo server

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+//connect to mongoose
+mongoose
+  .connect("mongodb://localhost/savetheanimals", { useNewUrlParser: true })
+  .then(() => console.log("mongoDB Connected ..."))
+  .catch(err => console.log(err));
+//
 
 app.get("/", (req, res) => {
   res.send("API is running ~ :)");
