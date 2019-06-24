f4a61fefe4a500ee3260d6845bd19c92dbb20ef9
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-24 00:33:26 -0400

    tweaked userSchema

--- users/user.model.js
+++ users/user.model.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const schema = new Schema({
+const userSchema = new Schema({
   username: { type: String, unique: true, required: true },
+  email: { type: String, required: true },
   hash: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true }
+  fullName: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true }
+  }
 });
 
-schema.set("toJSON", { virtuals: true });
+userSchema.set("toJSON", { virtuals: true });
 
-module.exports = mongoose.model("User", schema);
+module.exports = mongoose.model("User", userSchema);
