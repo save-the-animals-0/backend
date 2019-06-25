4855e8fbc2afd063f3eea4438131da1dc36356f4
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-25 18:11:16 -0400

    added missing fields from mvp for location, urgency, species

--- models/campaign.js
+++ models/campaign.js
@@ -5,6 +5,9 @@ const campaignSchema = new Schema({
   campaignName: { type: String, unique: true, required: true },
   fundingGoal: { type: Number, unique: false, required: true },
   deadline: { type: Date, unique: false, required: true },
+  location: { type: String, unique: false, required: true },
+  urgencyLevel: { type: String, unique: false, required: true },
+  species: { type: String, unique: false, required: true },
   description: { type: String, unique: false, required: true }
 });
 
