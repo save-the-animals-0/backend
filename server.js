f7cb05651bad1017ee0a76132558ed149a23bc4a
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-25 18:58:31 -0400

    added a get for api test

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ require("./database");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
+app.get("/", (req, res) => res.status(200).json({ api: "api running" }));
 app.use("/users", require("./controllers/user"));
 app.use("/campaigns", require("./controllers/campaign"));
 app.use(errorHandler);
+
+
 
 const port =
   process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 8000;
