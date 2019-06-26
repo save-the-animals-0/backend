cb0100a36b9a047c2c40ac7b654c95e8eb5fdc8c
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-06-25 21:44:52 -0400

    changed mongo uri from mongo-atlas to mlab heroku addon

--- database/index.js
+++ database/index.js
@@ -7,7 +7,8 @@ const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 
 // const mongoURI = `mongodb+srv://${DB_USER}:${DB_PASS}@save-the-animals-dzt5e.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-const mongoURI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-a4whc.mongodb.net/test?retryWrites=true&w=majority`
+// const mongoURI = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-a4whc.mongodb.net/test?retryWrites=true&w=majority`
+const mongoURI = `mongodb://heroku_4f8pzv62:m9g4gbjugni5o3aftbajcnqs2f@ds243607.mlab.com:43607/heroku_4f8pzv62`
 
 mongoose.connect(
   mongoURI,
