bd989c55bf3d09d27a1909e1260f8f8d98f4dae1
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-27 16:11:43 -0400

    Updated a req from the campaign schema

--- models/campaign.js
+++ models/campaign.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const campaignSchema = new Schema({
-  campaignName: { type: String, unique: true, required: true },
+  campaignName: { type: String, unique: false, required: true },
   fundingGoal: { type: Number, unique: false, required: true },
   fundingRaised: { type: Number, unique: false, required: false },
   deadline: { type: Date, unique: false, required: true },
