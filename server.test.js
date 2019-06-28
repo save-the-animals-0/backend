e4e6eee4ffb4f1addbf6a6c5e28394e60974b4ac
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-28 00:15:52 -0400

    Wrote server.js tests

--- server.test.js
+++ server.test.js
@@ -1,15 +1,31 @@
 const request = require("supertest");
 const app = require("./server");
 
-describe("GET [/]", () => {
-  it("should return a 200 status code", async () => {
-    const res = await request(app).get("/");
-    expect(res.status).toBe(200);
-  });
+describe("server.js", () => {
+  describe("index route", () => {
+    it("test env production port", async () => {
+      expect(process.env.NODE_ENV).toEqual("production");
+    });
 
-  it("should return JSON response", async () => {
-    const res = await request(app).get("/");
-    expect(res.type).toEqual("application/json");
+    it("test env local port", async () => {
+      expect(process.env.PORT).toEqual("80 : 8000");
+    });
+
+    it("should return an OK status code from the index route", async () => {
+      const expectedStatusCode = 200;
+      const response = await request(app).get("/");
+      expect(response.status).toEqual(expectedStatusCode);
+    });
+
+    it("should return a JSON object from the index route", async () => {
+      const expectedBody = { api: "api is running" };
+      const response = await request(app).get("/");
+      expect(response.body).toEqual(expectedBody);
+    });
+
+    it("should return a JSON object from the index route", async () => {
+      const response = await request(app).get("/");
+      expect(response.type).toEqual("application/json");
+    });
   });
 });
-
